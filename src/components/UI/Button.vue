50e3d00ccfcbd7769872db4ac8bde7db4eae2d52
vmwebio <vmwebio@gmail.com>
2024-08-03 20:35:03 +0300

    feat(components): добавил кнопку - Primary

---
+++ src/components/UI/Button.vue
@@ -0,0 +1,28 @@
+<template>
+    <button class="btn">
+      <slot>Button</slot>
+    </button>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Button',
+  };
+  </script>
+  
+  <style scoped>
+  .btn {
+    background-color: #6200ea;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    cursor: pointer;
+    font-size: 16px;
+    border-radius: 4px;
+  }
+  
+  .btn:hover {
+    background-color: #3700b3;
+  }
+  </style>
+  
