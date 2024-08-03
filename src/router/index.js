253d74158af6876ee4c734d7422dff2aa936fbe7
vmwebio <vmwebio@gmail.com>
2024-08-03 19:49:36 +0300

    feat: добавил настройки Роутера

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import Elements from '../views/Elements.vue';
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/elements/:element', component: Elements }
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-    },
-    {
-      path: '/about',
-      name: 'about',
+  history: createWebHistory(),
+  routes,
+});
 
-    }
-  ]
-})
-
-export default router
+export default router;
