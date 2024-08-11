c3fc77de0d62ae2ee27bf5e730818d256a185ec8
vmwebio <vmwebio@gmail.com>
2024-08-11 09:05:05 +0300

    update(src>main.js): импорт font-awesome

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import './assets/main.css'
 
 import '@mdi/font/css/materialdesignicons.css';
+import FontAwesomeIcon from './plugins/fontawesome';
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.use(router)
 
