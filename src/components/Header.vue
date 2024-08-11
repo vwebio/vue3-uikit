b24c50748794ed1c9c5339b5521644f8567734ec
vmwebio <vmwebio@gmail.com>
2024-08-11 10:03:39 +0300

    update(components>header): обновил ссылку заголовка

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="header">
-      <h1><a href="/" title="Home">UI Kit - VUE 3</a></h1>
+      <h1><a href="/vue3-uikit/" title="Home">UI Kit - VUE 3</a></h1>
     </header>
   </template>
   
