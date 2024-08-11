02737bf99046cd64d1b2b8fb5bf6a3503c4670e8
vmwebio <vmwebio@gmail.com>
2024-08-11 10:17:34 +0300

    update(components>header): обновил текст в Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
     <header class="header">
       <h1><a href="/vue3-uikit/" title="Home">UI Kit - VUE 3</a></h1>
+      <p>Выберите элемент на боковой панели</p>
     </header>
   </template>
   
